5d20be7d6ced8fd72b9fa36d5ac5bae3e1db9bb9
benjini123 <benjahenley@hotmail.com>
2022-12-16 17:16:22 -0300

    tranki git guardian

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -1,5 +1,6 @@
 import Airtable from "airtable";
+import "dotenv/config";
 
-export const base = new Airtable({ apiKey: "keyAkYdJC7btqS1zH" }).base(
-  "appPUPnBKb3ybjIYP"
+export const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_API_BASE as string
 );
