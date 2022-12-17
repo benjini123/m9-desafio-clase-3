f64fcfd3e682f49e2398862195b3355208a0059e
benjini123 <benjahenley@hotmail.com>
2022-12-17 16:37:17 -0300

    correccion

--- pages/index.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
-
-export default methods({
-  async get(req: NextApiRequest, res: NextApiResponse) {
-    res.json({ message: "done" });
-  },
-});
