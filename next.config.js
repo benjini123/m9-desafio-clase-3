a20c6b154b21426df006c86c1c049d0de5ab069c
benjini123 <benjahenley@hotmail.com>
2022-12-17 16:15:49 -0300

    correccion

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
+
+module.exports = nextConfig;
+
+module.exports = {
   env: {
     API_KEY_ALGOLIA: "8b530da2581ed56ffc453104c1734878",
     AIRTABLE_API_KEY: "keyAkYdJC7btqS1zH",
     AIRTABLE_API_BASE: "appPUPnBKb3ybjIYP",
   },
 };
-
-module.exports = nextConfig;
