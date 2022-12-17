2c7ba42f898704875580c7cfe6fd528ea4f82422
benjini123 <benjahenley@hotmail.com>
2022-12-17 16:24:02 -0300

    correccion

--- lib/algolia.ts
+++ lib/algolia.ts
@@ -1,9 +1,6 @@
 import algoliasearch from "algoliasearch";
 import "dotenv/config";
 
-const client = algoliasearch(
-  "K98RO3JPW0",
-  process.env.API_KEY_ALGOLIA as string
-);
+const client = algoliasearch("K98RO3JPW0", "8b530da2581ed56ffc453104c1734878");
 
 export const index = client.initIndex("products");
