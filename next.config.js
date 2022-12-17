1359f37b60c56ef5589c4f7b08f5ee286c930940
benjini123 <benjahenley@hotmail.com>
2022-12-17 16:21:40 -0300

    correccion

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-module.exports = {
-  env: {
-    API_KEY_ALGOLIA: "8b530da2581ed56ffc453104c1734878",
-    AIRTABLE_API_KEY: "keyAkYdJC7btqS1zH",
-    AIRTABLE_API_BASE: "appPUPnBKb3ybjIYP",
-  },
-};
+// module.exports = {
+//   env: {
+//     API_KEY_ALGOLIA: "8b530da2581ed56ffc453104c1734878",
+//     AIRTABLE_API_KEY: "keyAkYdJC7btqS1zH",
+//     AIRTABLE_API_BASE: "appPUPnBKb3ybjIYP",
+//   },
+// };
